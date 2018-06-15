4c4879688c4b672ff3fb8f23907f56ac8cded7e1
h3adHunter <juanmanuelbaez1994@gmail.com>
2018-06-15 14:08:55 -0300

    Obtiene las Materias desde la API y define una tabla para su visualización

--- WebContent/controller/Principal.controller.js
+++ WebContent/controller/Principal.controller.js
@@ -3,6 +3,7 @@ sap.ui.controller(
     {
     	onAfterRendering: function() {     	   
     		this.cargarModelAulas();
+    		this.cargarModelMaterias();
     	},
     	
     	cargarModelAulas: function(){
@@ -19,6 +20,27 @@ sap.ui.controller(
                 	   oModelAulas.setData(data);
                        console.log(oModelAulas);   
                        oView.setModel(oModelAulas, "aulas");
+                   },
+                   error : function(jqXHR,textStatus,errorThrown) {
+                	  console.log("Error en la conexion");
+                   }
+    	    });
+    	},
+    	
+    	cargarModelMaterias: function(){
+    		var oView = this.getView();
+
+    	    var oModelMaterias = new sap.ui.model.json.JSONModel();
+
+    	    var url = 'http://127.0.0.1:8000/api/materias';
+    	    $.get({
+                   url : url,
+                   headers: { "Accept": "application/json"},
+   		           dataType: 'json',
+                   success : function(data, textStatus, jqXHR) {
+                	   oModelMaterias.setData(data);
+                       console.log(oModelMaterias);   
+                       oView.setModel(oModelMaterias, "materias");
                    },
                    error : function(jqXHR,textStatus,errorThrown) {
                 	  console.log("Error en la conexion");
