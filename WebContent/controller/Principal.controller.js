fa1e106945c26d307240e7a12c969ffd9c3ff88e
h3adHunter <juanmanuelbaez1994@gmail.com>
2018-06-15 12:19:48 -0300

    Permite visualizar las aulas obtenidas mediante metodo GET de ajax hacia la API

--- WebContent/controller/Principal.controller.js
+++ WebContent/controller/Principal.controller.js
@@ -1,6 +1,29 @@
 sap.ui.controller(
     "ar.com.unq.controller.Principal",
     {
+    	onAfterRendering: function() {     	   
+    		this.cargarModelAulas();
+    	},
     	
+    	cargarModelAulas: function(){
+    		var oView = this.getView();
+
+    	    var oModelAulas = new sap.ui.model.json.JSONModel();
+
+    	    var url = 'http://127.0.0.1:8000/api/aulas';
+    	    $.get({
+                   url : url,
+                   headers: { "Accept": "application/json"},
+   		           dataType: 'json',
+                   success : function(data, textStatus, jqXHR) {
+                	   oModelAulas.setData(data);
+                       console.log(oModelAulas);   
+                       oView.setModel(oModelAulas, "aulas");
+                   },
+                   error : function(jqXHR,textStatus,errorThrown) {
+                	  console.log("Error en la conexion");
+                   }
+    	    });
+    	}	
     }
 );
