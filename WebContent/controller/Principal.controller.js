11ea5b171d0a18d15d97961b1214ac4b7a29070d
h3adHunter <juanmanuelbaez1994@gmail.com>
2018-06-28 16:04:41 -0300

    Agrega tab de cursadas

--- WebContent/controller/Principal.controller.js
+++ WebContent/controller/Principal.controller.js
@@ -4,6 +4,7 @@ sap.ui.controller(
     	onAfterRendering: function() {     	   
     		this.cargarModelAulas();
     		this.cargarModelMaterias();
+    		this.cargarModelCursadas();
     	},
     	
     	cargarModelAulas: function(){
@@ -46,6 +47,28 @@ sap.ui.controller(
                 	  console.log("Error en la conexion");
                    }
     	    });
-    	}	
+    	},
+    	
+    	cargarModelCursadas: function(){
+    		var oView = this.getView();
+
+    	    var oModelCursadas = new sap.ui.model.json.JSONModel();
+
+    	    var url = 'http://127.0.0.1:8000/api/cursadas';
+    	    $.get({
+                   url : url,
+                   headers: { "Accept": "application/json"},
+   		           dataType: 'json',
+                   success : function(data, textStatus, jqXHR) {
+                	   oModelCursadas.setData(data);
+                       console.log(oModelCursadas);   
+                       oView.setModel(oModelCursadas, "cursadas");
+                   },
+                   error : function(jqXHR,textStatus,errorThrown) {
+                	  console.log("Error en la conexion");
+                   }
+    	    });
+    	}
+    	
     }
 );
